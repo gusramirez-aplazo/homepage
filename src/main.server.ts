c9ac67a6220411fb4d12dd41b992244d971a1102
Gus Ramírez <gustavo@aplazo.mx>
2024-04-30 17:37:39 -0600

    fix: render with base href

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,12 +1,13 @@
-import 'zone.js/node';
-import '@angular/platform-server/init';
-
+import { APP_BASE_HREF } from '@angular/common';
 import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
 import { renderApplication } from '@angular/platform-server';
-
-import { config } from './app/app.config.server';
+import '@angular/platform-server/init';
+import 'zone.js/node';
 import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
+
+const baseHref = process.env['CF_PAGES_URL'] ?? `http://localhost:8888`;
 
 if (import.meta.env.PROD) {
   enableProdMode();
@@ -19,7 +20,8 @@ export function bootstrap() {
 export default async function render(url: string, document: string) {
   const html = await renderApplication(bootstrap, {
     document,
-    url,
+    url: `${baseHref}${url}`,
+    platformProviders: [{ provide: APP_BASE_HREF, useValue: baseHref }],
   });
 
   return html;
